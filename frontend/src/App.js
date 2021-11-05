ecb933556542ee85af78fdd46c2f1b11dcbfc4b0
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-11-05 11:18:49 -0300

    Criando funcionalidades de atualizar e deletar tarefa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,11 +12,12 @@ function getData() {
   .then(response => response.json())
   .then(data => setTasks(data))
 }
+
 function inserirTarefa() {
   fetch('http://localhost:3000/todo',
   {
     method: "POST",
-    headers: { 'Content-Type': "application/json"},
+    headers: { 'Content-Type': "application/json" },
     body: JSON.stringify({ "task": "", "active": true })})
   .then(response => response.json())
   .then(() => getData())
@@ -48,13 +49,12 @@ useEffect(() => {
   getData();
 }, []);
 
-
   return (
     <div className="App">
       <h1 className="h1">Organizador de Tarefas</h1>
 
-      { tasks.map(task => {
-        return <Tarefa item={ task } key={ task._id } atualizarTarefa={ atualizarTarefa } deletarTarefa={ deletarTarefa }/>
+      { tasks.map(item => {
+        return <Tarefa item={ item } key={ item.id } atualizarTarefa={ atualizarTarefa } deletarTarefa={ deletarTarefa }/>
       }) }
 
       <button className="btnTodas">Todas as tarefas</button>
--- frontend/src/components/Tarefa.jsx
+++ frontend/src/components/Tarefa.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import { useState, React } from 'react';
 import PropTypes from 'prop-types';
 import '../App.css';
 
-function Tarefa({ item, atualizarTarefa }) {
+function Tarefa({ item, atualizarTarefa, deletarTarefa }) {
+
+  const [temtask, setTempTask] = useState("");
 
   return (
     <div className="main">
-      <input type="checkbox" checked={ !item.active } onClick={() => {atualizarTarefa({...item, active: !item.active }) } }/>
-      <span>{ item.task }</span>
-      <button>Apagar</button>
+      <input
+        type="checkbox"
+        checked={ !item.active }
+        onClick={() => { atualizarTarefa({...item, active: !item.active }) } }
+      />
+
+      { ((item.edit) || (item.task === "")) ? (
+        <input
+          type="text"
+          placeholder={item.task}
+          onChange={(e) => { setTempTask(e.target.value) } }
+          onBlur={() => { atualizarTarefa({...item, task: temtask, edit: false }) } }
+          />
+      )
+        :
+      <span
+        onClick={() => { atualizarTarefa({ ...item, edit:true }) } }
+        style={item.active ? {} : { textDecoration: "line-through" } }
+      >{ item.task }</span>
+    }
+      <button onClick={() => { deletarTarefa(item) } }>Apagar</button>
   </div>
   );
 }
@@ -16,5 +36,6 @@ function Tarefa({ item, atualizarTarefa }) {
 Tarefa.propTypes = {
   item: PropTypes.object,
   atualizarTarefa: PropTypes.func,
+  deletarTarefa: PropTypes.func,
 };
 export default Tarefa;
